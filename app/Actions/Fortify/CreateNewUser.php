c92b61a3c51d4ed80ec42afed76dca5ed45b0dfc
iliao <iliaosipov.134@gmail.com>
2023-03-26 20:07:43 +0300

    Fixes

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -26,6 +26,7 @@ class CreateNewUser implements CreatesNewUsers
 
         return User::create([
             'name' => $input['name'],
+            'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
     }
